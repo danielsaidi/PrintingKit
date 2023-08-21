c869b9114bb13b3e015b98102e874a0f618df3bf
Daniel Saidi <daniel.saidi@gmail.com>
2023-08-21 14:24:05 +0200

    Add demo app

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Daniel Saidi on 2023-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
