586cc34b9eacab24ad3ab90c96ede5889040f6ec
Daniel Saidi <daniel.saidi@gmail.com>
2026-01-06 09:16:10 +0100

    Update copyright info

--- Sources/PrintingKit/Extensions/Image+Printing.swift
+++ Sources/PrintingKit/Extensions/Image+Printing.swift
@@ -3,7 +3,7 @@
 //  PrintingKit
 //
 //  Created by Daniel Saidi on 2025-04-01.
-//  Copyright © 2025 Daniel Saidi. All rights reserved.
+//  Copyright © 2025-2026 Daniel Saidi. All rights reserved.
 //
 
 import SwiftUI
